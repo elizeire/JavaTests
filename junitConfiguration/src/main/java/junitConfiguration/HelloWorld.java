dbfcb4091d28708b02bc03c8f8aa7f6daaf2fd02
elizeire@gmail.com <elizeire@gmail.com>
2017-05-13 22:53:37 +0200

    example of ignoring some tests on maven build

---
+++ junitConfiguration/src/main/java/junitConfiguration/HelloWorld.java
@@ -0,0 +1,9 @@
+package junitConfiguration;
+
+public class HelloWorld {
+	
+	public String sayHello(String name){
+		return "hello "+name;
+	}
+
+}
---
+++ junitConfiguration/src/test/java/com/example/tests/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package com.example.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junitConfiguration.HelloWorld;
+
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		HelloWorld helloWorld = new HelloWorld();
+		String hello = helloWorld.sayHello("Kirill");
+		assertEquals("hello Kirill", hello);
+	}
+
+}
